3033fcd1345b2dca89740c22b6a675792dc5144c
jw.choi <choijw9808@bnosoft.co.kr>
2023-10-30 16:40:13 +0900

    h2 database 구축 및 vue.js nuxt기반 front 환경 구축

---
+++ front/nuxt.config.ts
@@ -0,0 +1,38 @@
+export default defineNuxtConfig({
+  ssr: false,
+  pages: true,
+  components: true,
+  imports: {
+    autoImport: true
+  },
+  app:{
+    buildAssetsDir: "_nuxt",
+    head: {
+      title: "cjw_example",
+      htmlAttrs: { lang: "ko" },
+      charset: "utf-8",
+      link: [
+        {
+          rel: "icon",
+          type: "image/png",
+          href: "/favicon.png",
+        },
+      ],
+    }
+  },
+  css: [
+    //"@progress/kendo-theme-default/dist/all.css", kendoUi 사용시 주석해제
+    "vuetify/lib/styles/main.sass"
+  ],
+  modules: [
+    "@pinia/nuxt",
+    "@pinia-plugin-persistedstate/nuxt",
+    "@invictus.codes/nuxt-vuetify",
+  ],
+  nitro: {
+  },
+  vuetify: {
+  },
+  vite: {
+  },
+});
