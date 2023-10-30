341b213aed2898a865c6bdb69787ad88bb7a466c
jw.choi <choijw9808@bnosoft.co.kr>
2023-10-30 09:25:35 +0900

    Initial commit

---
+++ src/main/java/com/example/cjw_example/CjwExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.cjw_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CjwExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CjwExampleApplication.class, args);
+	}
+
+}
