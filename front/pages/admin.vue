b0dd4721f984c54bd5ffb81f24fc3853f4e80ed7
최주욱 <choijw9808@bnosoft.co.kr>
2023-11-01 17:42:38 +0900

    [front] 관리자정보 리스트 기능 개선

--- front/pages/admin.vue
+++ front/pages/admin.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import {useFetchApi} from "~/composables/useFetchApi";
 
-const { data } = await useFetchApi('/admin')
+const { data : admin, pending, error } = await useFetchApi('/admin')
 </script>
 
 <template>
@@ -10,6 +10,19 @@ const { data } = await useFetchApi('/admin')
             ADMIN 관리자 정보
         </h1>
 
-        <pre class="text-left text-xs">{{ data }}</pre>
+        <p v-if="pending">Fetching...</p>
+        <pre v-else-if="error">관리자정보 조회 불가 : {{ error.data }}</pre>
+        <table v-else class="admin">
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">관리자명</th>
+                <th scope="col">부서명</th>
+            </tr>
+            <tr v-for="item in admin">
+                <td>{{ item.id }}</td>
+                <td>{{ item.name }}</td>
+                <td>{{ item.deptname }}</td>
+            </tr>
+        </table>
     </NuxtExampleLayout>
 </template>
