7534bd3ad1a758944b12fea5294a1033632b8a9d
최주욱 <choijw9808@bnosoft.co.kr>
2023-11-01 17:29:37 +0900

    [front, backend] 상품관리 리스트 기능 구현

--- front/pages/product.vue
+++ front/pages/product.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { useFetchApi} from "~/composables/useFetchApi";
 
-const { data } = await useFetchApi('/product')
+const { data : product, pending, error } = await useFetchApi('/product')
 </script>
 
 <template>
@@ -10,7 +10,22 @@ const { data } = await useFetchApi('/product')
             ADMIN 상품 관리
         </h1>
 
-        <pre class="text-left text-xs">{{ data }}</pre>
+        <p v-if="pending">Fetching...</p>
+        <pre v-else-if="error">상품 조회 불가 : {{ error.data }}</pre>
+        <table v-else class="product">
+            <tr>
+                <th scope="col">ID</th>
+                    <th scope="col">상품명</th>
+                        <th scope="col">상품 상세정보</th>
+                            <th scope="col">금액</th>
+            </tr>
+            <tr v-for="item in product">
+                <td>{{ item.id }}</td>
+                <td>{{ item.name }}</td>
+                <td>{{ item.detail }}</td>
+                <td>{{ item.price }}</td>
+            </tr>
+        </table>
     </NuxtExampleLayout>
 </template>
 
